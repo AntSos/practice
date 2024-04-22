636a11def6a3230c9274056f93c8c86b379ba340
Antonio Sosa <sosanaoh@hotmail.com>
2024-04-21 18:50:59 -0600

    A simple program tha returns the steps and number to reach the collatz conjeture 2024/04/20

---
+++ Lothar_Collatz_n.py
@@ -0,0 +1,19 @@
+#Asimple program that count the steps to reach number using Collatz teory
+
+def l_c_steps (number):
+    count = 0
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = (number * 3) + 1
+
+        count += 1
+    print (f"Steps to Lothar Collatz conjeture = {count}")
+
+def main ():
+    number = int (input ("Enter a number: "))
+    l_c_steps (number)
+
+if __name__ == "__main__":
+    main ()
