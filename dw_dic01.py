7c0f9fa4e9b2c5f31aa486f5dc90efe8cda070fb
Antonio Sosa <sosanaoh@hotmail.com>
2024-04-28 14:29:39 -0600

    Datawars dictionary excersise 01 2024-04-28

---
+++ dw_dic01.py
@@ -0,0 +1,264 @@
+players_data = {'LeBron James': {'rating': 97,
+  'jersey': '#23',
+  'team': 'Los Angeles Lakers',
+  'position': 'F',
+  'b_day': '12/30/84',
+  'height': '6-9 / 2.06',
+  'weight': '250 lbs. / 113.4 kg.',
+  'salary': '$37436858',
+  'country': 'USA',
+  'draft_year': 2003,
+  'draft_round': 1,
+  'draft_peak': 1,
+  'college': "nan",
+  'version': 'NBA2k20'},
+ 'Kawhi Leonard': {'rating': 97,
+  'jersey': '#2',
+  'team': 'Los Angeles Clippers',
+  'position': 'F',
+  'b_day': '06/29/91',
+  'height': '6-7 / 2.01',
+  'weight': '225 lbs. / 102.1 kg.',
+  'salary': '$32742000',
+  'country': 'USA',
+  'draft_year': 2011,
+  'draft_round': 1,
+  'draft_peak': 15,
+  'college': 'San Diego State',
+  'version': 'NBA2k20'},
+ 'Giannis Antetokounmpo': {'rating': 96,
+  'jersey': '#34',
+  'team': 'Milwaukee Bucks',
+  'position': 'F-G',
+  'b_day': '12/06/94',
+  'height': '6-11 / 2.11',
+  'weight': '242 lbs. / 109.8 kg.',
+  'salary': '$25842697',
+  'country': 'Greece',
+  'draft_year': 2013,
+  'draft_round': 1,
+  'draft_peak': 15,
+  'college': "nan",
+  'version': 'NBA2k20'},
+ 'Kevin Durant': {'rating': 96,
+  'jersey': '#7',
+  'team': 'Brooklyn Nets',
+  'position': 'F',
+  'b_day': '09/29/88',
+  'height': '6-10 / 2.08',
+  'weight': '230 lbs. / 104.3 kg.',
+  'salary': '$37199000',
+  'country': 'USA',
+  'draft_year': 2007,
+  'draft_round': 1,
+  'draft_peak': 2,
+  'college': 'Texas',
+  'version': 'NBA2k20'},
+ 'James Harden': {'rating': 96,
+  'jersey': '#13',
+  'team': 'Houston Rockets',
+  'position': 'G',
+  'b_day': '08/26/89',
+  'height': '6-5 / 1.96',
+  'weight': '220 lbs. / 99.8 kg.',
+  'salary': '$38199000',
+  'country': 'USA',
+  'draft_year': 2009,
+  'draft_round': 1,
+  'draft_peak': 3,
+  'college': 'Arizona State',
+  'version': 'NBA2k20'},
+ 'Stephen Curry': {'rating': 95,
+  'jersey': '#30',
+  'team': 'Golden State Warriors',
+  'position': 'G',
+  'b_day': '03/14/88',
+  'height': '6-3 / 1.91',
+  'weight': '185 lbs. / 83.9 kg.',
+  'salary': '$40231758',
+  'country': 'USA',
+  'draft_year': 2009,
+  'draft_round': 1,
+  'draft_peak': 7,
+  'college': 'Davidson',
+  'version': 'NBA2k20'},
+ 'Anthony Davis': {'rating': 94,
+  'jersey': '#3',
+  'team': 'Los Angeles Lakers',
+  'position': 'F-C',
+  'b_day': '03/11/93',
+  'height': '6-10 / 2.08',
+  'weight': '222 lbs. / 100.7 kg.',
+  'salary': '$27093019',
+  'country': 'USA',
+  'draft_year': 2012,
+  'draft_round': 1,
+  'draft_peak': 1,
+  'college': 'Kentucky',
+  'version': 'NBA2k20'},
+ 'Paul George': {'rating': 93,
+  'jersey': '#13',
+  'team': 'Los Angeles Clippers',
+  'position': 'F',
+  'b_day': '05/02/90',
+  'height': '6-8 / 2.03',
+  'weight': '210 lbs. / 95.3 kg.',
+  'salary': '$33005556',
+  'country': 'USA',
+  'draft_year': 2010,
+  'draft_round': 1,
+  'draft_peak': 10,
+  'college': 'Fresno State',
+  'version': 'NBA2k20'},
+ 'Damian Lillard': {'rating': 92,
+  'jersey': '#0',
+  'team': 'Portland Trail Blazers',
+  'position': 'G',
+  'b_day': '07/15/90',
+  'height': '6-2 / 1.88',
+  'weight': '195 lbs. / 88.5 kg.',
+  'salary': '$29802321',
+  'country': 'USA',
+  'draft_year': 2012,
+  'draft_round': 1,
+  'draft_peak': 6,
+  'college': 'Weber State',
+  'version': 'NBA2k20'},
+ 'Joel Embiid': {'rating': 91,
+  'jersey': '#21',
+  'team': 'Philadelphia 76ers',
+  'position': 'C',
+  'b_day': '03/16/94',
+  'height': '7-0 / 2.13',
+  'weight': '250 lbs. / 113.4 kg.',
+  'salary': '$27504630',
+  'country': 'Cameroon',
+  'draft_year': 2014,
+  'draft_round': 1,
+  'draft_peak': 3,
+  'college': 'Kansas',
+  'version': 'NBA2k20'},
+ 'Kyrie Irving': {'rating': 91,
+  'jersey': '#11',
+  'team': 'Brooklyn Nets',
+  'position': 'G',
+  'b_day': '03/23/92',
+  'height': '6-2 / 1.88',
+  'weight': '180 lbs. / 81.6 kg.',
+  'salary': '$31742000',
+  'country': 'Australia',
+  'draft_year': 2011,
+  'draft_round': 1,
+  'draft_peak': 1,
+  'college': 'Duke',
+  'version': 'NBA2k20'},
+ 'Nikola Jokic': {'rating': 90,
+  'jersey': '#15',
+  'team': 'Denver Nuggets',
+  'position': 'C',
+  'b_day': '02/19/95',
+  'height': '7-0 / 2.13',
+  'weight': '253 lbs. / 114.8 kg.',
+  'salary': '$27504630',
+  'country': 'Serbia',
+  'draft_year': 2014,
+  'draft_round': 2,
+  'draft_peak': 41,
+  'college': "nan",
+  'version': 'NBA2k20'},
+ 'Russell Westbrook': {'rating': 90,
+  'jersey': '#0',
+  'team': 'Houston Rockets',
+  'position': 'G',
+  'b_day': '11/12/88',
+  'height': '6-3 / 1.91',
+  'weight': '190 lbs. / 86.2 kg.',
+  'salary': '$38506482',
+  'country': 'USA',
+  'draft_year': 2008,
+  'draft_round': 1,
+  'draft_peak': 4,
+  'college': 'UCLA',
+  'version': 'NBA2k20'},
+ 'Klay Thompson': {'rating': 89,
+  'jersey': '#11',
+  'team': 'Golden State Warriors',
+  'position': 'G',
+  'b_day': '02/08/90',
+  'height': '6-6 / 1.98',
+  'weight': '205 lbs. / 93 kg.',
+  'salary': '$32742000',
+  'country': 'USA',
+  'draft_year': 2011,
+  'draft_round': 1,
+  'draft_peak': 11,
+  'college': 'Washington State',
+  'version': 'NBA2k20'}}
+
+player_stats = {'LeBron James': {'rating': 97, 'jersey': '#23', 'team': 'Los Angeles Lakers', 'position': 'F', 'b_day': '12/30/84', 'height': '6-9 / 2.06', 'weight': '250 lbs. / 113.4 kg.', 'country': 'USA', 'draft_year': 2003, 'draft_round': 1, 'draft_peak': 1, 'college': None},
+                'Kawhi Leonard': {'rating': 97, 'jersey': '#2', 'team': 'Los Angeles Clippers', 'position': 'F', 'b_day': '06/29/91', 'height': '6-7 / 2.01', 'weight': '225 lbs. / 102.1 kg.', 'country': 'USA', 'draft_year': 2011, 'draft_round': 1, 'draft_peak': 15, 'college': 'San Diego State'},
+                'Giannis Antetokounmpo': {'rating': 96, 'jersey': '#34', 'team': 'Milwaukee Bucks', 'position': 'F-G', 'b_day': '12/06/94', 'height': '6-11 / 2.11', 'weight': '242 lbs. / 109.8 kg.', 'country': 'Greece', 'draft_year': 2013, 'draft_round': 1, 'draft_peak': 15, 'college': None},
+                'Kevin Durant': {'rating': 96, 'jersey': '#7', 'team': 'Brooklyn Nets', 'position': 'F', 'b_day': '09/29/88', 'height': '6-10 / 2.08', 'weight': '230 lbs. / 104.3 kg.', 'country': 'USA', 'draft_year': 2007, 'draft_round': 1, 'draft_peak': 2, 'college': 'Texas'},
+                'James Harden': {'rating': 96, 'jersey': '#13', 'team': 'Houston Rockets', 'position': 'G', 'b_day': '08/26/89', 'height': '6-5 / 1.96', 'weight': '220 lbs. / 99.8 kg.', 'country': 'USA', 'draft_year': 2009, 'draft_round': 1, 'draft_peak': 3, 'college': 'Arizona State'}}
+
+
+player_salaries = {'LeBron James': '$37436858', 'Kawhi Leonard': '$32742000', 'Giannis Antetokounmpo': '$25842697', 'Kevin Durant': '$37199000', 'James Harden': '$38199000'}
+
+
+#Access the values using key
+lebron_height = players_data['LeBron James']['height']
+
+# Add a new player to the player data dictionary
+players_data ["Stephen Curry"] = {'jersey': 30,
+ 'team': 'Golden State Warriors',
+ 'position': 'PG',
+ 'rating': 97,
+ 'salary': 40231758,
+ 'height': '6-3 / 1.91',
+ 'weight': '185 lbs',
+ 'dob': 'March 14, 1988',
+ 'country': 'USA',
+ 'college': 'Davidson',
+ 'draft_year': 2009,
+ 'draft_round': 1,
+ 'draft_peak': 7} 
+
+
+#Access the keys and values of dictionary
+players_names = list(players_data.keys())
+players_details = list(players_data.values())
+
+#Create a new dictionary 
+players_college = {}
+for name, details in players_data.items():
+    players_college[name] = details['college']
+
+# Check if value is present or not in the dictionary
+is_in = 'Klay Thompson' in players_data
+
+#Merge two dictionaries
+players_data_merged = {}
+for player in player_stats:
+    players_data_merged[player] = player_stats[player]
+    if player in player_salaries:
+        players_data_merged[player]['salary'] = player_salaries[player]
+
+
+
+# Update the values
+players_data = players_data ['LeBron James']. update ({'rating': 95, 'salary': '$34,000,000'}) 
+
+# Create a new specific dictionary
+players_data_usa = {}
+for player in players_data:
+    if players_data[player]['country'] == 'USA':
+        players_data_usa[player] = [players_data[player]['rating'], players_data[player]['team']]
+
+#Create a new dictionary with counts
+teams = {}
+for player in players_data:
+    if players_data[player]['team'] in teams:
+        teams[players_data[player]['team']] += 1
+    else:
+        teams[players_data[player]['team']] = 1
+print (teams)
